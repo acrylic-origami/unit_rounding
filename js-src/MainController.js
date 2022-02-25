9b55a143981044681136a4fdcf171d81e2d15d8e
Derek Lam <dc.lam@mail.utoronto.ca>
2022-02-24 17:49:28 -0800

    More formatting

--- js-src/MainController.js
+++ js-src/MainController.js
@@ -17,7 +17,9 @@ export default class extends React.Component {
 			n_fulfilled: 0,
 			request: null,
 			err: null,
-			copying: false
+			copying: false,
+			show_long_units: true,
+			show_usage_notes: false,
 		};
 		window.addEventListener('popstate', this.handle_uri_term);
 	}
@@ -26,19 +28,26 @@ export default class extends React.Component {
 		this.handle_uri_term();
 	}
 	handle_uri_term = () => {
-		// const term_raw = (new URLSearchParams(window.location.search)).get('term_raw');
-		// if(term_raw != null) {
-		// 	this.term_raw_ref.current.value = term_raw;
-		// 	this.setState(s => ({ term_raw, n_request: s.n_request + 1 }));
-		// }
+		const U = new URLSearchParams(window.location.search);
+		const terms = ['unit', 'start', 'end'].map(k => U.get(`term_${k}`));
+		const [term_unit, term_start, term_end] = terms;
+		console.log(terms);
+		if(terms.reduce((a, b) => a && b !== null && b !== '', true)) {
+			this.setState({ term_unit, term_start, term_end }, _ => this.onSubmit());
+		}
 	}
 	
 	onSubmit = e => {
-		history.pushState({}, `search`, ['unit', 'start', 'end'].map(a => `?term_${a}=${encodeURI(this[`term_${a}_ref`].current.value)}`).join('&'));
+		history.pushState({}, `search`, '?' + ['unit', 'start', 'end'].map(a => `term_${a}=${encodeURI(this[`term_${a}_ref`].current.value)}`).join('&'));
 		this.setState(({ n_request }) => ({ n_request: n_request + 1 }));
-		e.preventDefault();
+		if(e !== undefined) {
+			e.preventDefault();
+			e.stopPropagation();
+		}
+		return false;
 	}
 	handleInput = (k, e) => this.setState({ [k]: e.target.value });
+	handleToggleShowLongUnits = e => this.setState({ show_long_units: e.target.checked })
 	
 	componentDidUpdate(_, l) {
 		if(this.state.err !== null && !this.state.err[1]) {
@@ -61,9 +70,11 @@ export default class extends React.Component {
 				.then(r => this.setState(s => (n_request_stash === s.n_request) && { result: r, n_fulfilled: n_request_stash }), fail);
 		}
 	}
+	
+	handleToggleShowUsageNotes = e => this.setState(s => ({ show_usage_notes: !s.show_usage_notes }))
 			
 	render = () => <div>
-		<section>
+		<section id="input_section">
 				<form onSubmit={this.onSubmit} id="term_form" ref={this.form_ref}>
 					{
 						(() => {
@@ -115,6 +126,7 @@ export default class extends React.Component {
 			}
 			{ this.state.result && 
 				<section id="results">
+					<div id="show_long_units_wrapper"><input type="checkbox" id="toggle_show_long_units" checked={this.state.show_long_units} onChange={this.handleToggleShowLongUnits} /><label htmlFor="toggle_show_long_units">Show long unit names</label></div>
 					<h2>{this.state.result.start} {this.state.result.unit} is:</h2>
 					<ul id="result_list">
 						{ this.state.result.path.map((n, i) =>
@@ -122,9 +134,9 @@ export default class extends React.Component {
 								<React.Fragment>
 									<span className="result-size">{ (n.fro / n.factor).toFixed(3) }</span>
 									&nbsp;<span className="result-unit">{ n.vto_unit }</span>
-									&nbsp;<span className="result-long-unit">({n.vto_unit_long})</span>
+									{ this.state.show_long_units && <span className="result-long-unit">&nbsp;({n.vto_unit_long})</span> }
 									&nbsp;<span className="result-rounding">
-										rounded to
+										<span style={{ fontSize: '1.4em' }}>&#10613;</span> {/*&asymp;*/}
 										&nbsp;<span className="result-size">{ Math.round(n.fro / n.factor) }</span>
 										&nbsp;<span className="result-unit">{ n.vto_unit }</span>
 											&nbsp;<span className="result-aka">
@@ -139,5 +151,22 @@ export default class extends React.Component {
 					<h2>&hellip;which is {Math.round(this.state.result.end)} {this.state.result.unit} (up to rounding error)</h2>
 				</section>
 			}
+			<section id="usage_notes">
+				<h2 onClick={this.handleToggleShowUsageNotes}>Usage notes { this.state.show_usage_notes ? <React.Fragment>&#x25BC;</React.Fragment> : <React.Fragment>&#x25c0;</React.Fragment> }</h2>
+				<ul className={ this.state.show_usage_notes ? '' : 'hidden' }>
+					<li><em>Entering units:</em> Any unit symbol available from the <a href="//en.wikipedia.org/wiki/Module:Convert/data">Wikipedia Conversion Tool</a> can be used, which includes most of the conventional and many unconventional units.</li>
+					<ul>
+						<li>Units can be multiplied by separating with spaces, *, or &middot;. Units can be divided by separating with / or &#247;.</li>
+						<li>Units can be bracketed, e.g. <code>km/(gal/day) * A</code></li>
+						<li>Where names conflict or aren't found, try spelling out the whole name, or fall back to SI units (s,kg,m,K,A,cd,mol)</li>
+					</ul>
+					<li><em>Intermediate units:</em> A few unit sets are available to use for rounding:</li>
+					<ol>
+						<li><em>Wikipedia units:</em> Only units directly found in the Wikipedia Conversion Tool can serve as intermediate units.</li>
+						<li><em>Exotic units:</em> All combinations of two units from the Wikipedia Conversion Tool can serve as intermediate units. This greatly improves the hitrate &mdash; the baseline set of units by itself is too sparse to converge much of the time</li>
+						<li><em>Cheating units:</em> Powers of a unitless unit are tacked onto the input term successively until it converges. The results aren't quite as interesting, but this is viable. The intermediate units are still in the same base as the original, just without much physical meaning.</li>
+					</ol>
+				</ul>
+			</section>
 	</div>
 }
